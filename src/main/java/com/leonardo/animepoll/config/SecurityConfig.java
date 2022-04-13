eb9fb79e2d54a99e48994bb6d73432dc58a13368
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2022-04-13 15:32:22 -0300

    Remoção das configurações do remote debug pois houveram conflitos com o CSRF

--- src/main/java/com/leonardo/animepoll/config/SecurityConfig.java
+++ src/main/java/com/leonardo/animepoll/config/SecurityConfig.java
@@ -1,22 +1,14 @@
 package com.leonardo.animepoll.config;
 
-import java.util.Arrays;
-
-import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
-
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
-    private final Environment env;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -26,16 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
             .and()
             
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-        if(Arrays.asList(env.getActiveProfiles()).contains("dev")) {
-            //Permite o acesso do Spring remote no profile dev
-            http
-                .authorizeRequests()
-                .antMatchers("/.~~spring-boot!~/**").permitAll()
-                .and()
-                .csrf().disable();
-        }
-
+            
     } 
 
 }
